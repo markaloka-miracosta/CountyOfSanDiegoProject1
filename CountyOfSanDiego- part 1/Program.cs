973aae1c9f3790dcabe23bca88f39feea3a1a707
Mark Aloka <maloka@miracosta.edu>
2020-08-24 12:42:20 -0700

    Add project files.

---
+++ CountyOfSanDiego- part 1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CountyOfSanDiego__part_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("this is my project ");
+
+        }
+    }
+}
